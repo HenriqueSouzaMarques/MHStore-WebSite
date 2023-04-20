005881c39e57bff53033e95640847e32e84ea664
HenriqueSouzaMarques <hesomarques@usp.br>
2023-04-20 01:30:36 -0300

    Implementando login/signin page

---
+++ js/login.js
@@ -0,0 +1,9 @@
+function showSignIn()
+{
+    document.getElementById("signin").classList.add("show");
+}
+
+function hideSignIn()
+{
+    document.getElementById("signin").classList.remove("show");
+}
