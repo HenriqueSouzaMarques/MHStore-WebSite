8e2447f50c79f20d58ddb71415d553178c6c04ee
HenriqueSouzaMarques <hesomarques@usp.br>
2023-05-07 23:46:04 -0300

    pequenas mudancas

--- js/login.js
+++ js/login.js
@@ -1,3 +1,11 @@
+"use strict";
+
+let buttonCreateAcc = document.getElementById("button-createacc");
+buttonCreateAcc.addEventListener("click", showSignIn);
+
+let buttonCloseCreateAcc = document.getElementById("button-close");
+buttonCloseCreateAcc.addEventListener("click", hideSignIn);
+
 function showSignIn()
 {
     document.getElementById("signin").classList.add("show");
